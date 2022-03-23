b4e5380931170697fee4ab8c830046860e7f8c4b
Rafael <rafaelm229@protonmail.com>
2022-03-23 18:44:58 -0300

    add conditional exercices

--- the-rust-programming-language/conditional/src/main.rs
+++ the-rust-programming-language/conditional/src/main.rs
@@ -12,4 +12,63 @@ fn main() {
     if number != 0{
         println!("number was something other than zero")
     }
+
+    let number = 6;
+    
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+    
+    let condition = true;
+    let number = if condition { 5 } else {6};
+    println!("The value number is {}",number);
+
+    let condition = true;
+
+    let number = if condition { 5 } else { "six" };
+
+    println!("The value of number is: {}", number);
+
+    loop {
+        println!("again!");
+    }
+
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
 }
